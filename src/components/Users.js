58522eac7d7bb63346ef5916d0a627901fb8d77b
Megan Horsfield <meganthadev@gmail.com>
2020-08-31 01:56:47 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -7,7 +7,7 @@ class Users extends Component {
       <div>
         <ul>
           Users!
-          {/* Write code here that displays the usernames of all users in the Redux store */}
+          let users = this.props.users.map(user => <li>{user.username}</li>)
           {/* In addition, display the total number of users curently in the store */}
         </ul>
       </div>
